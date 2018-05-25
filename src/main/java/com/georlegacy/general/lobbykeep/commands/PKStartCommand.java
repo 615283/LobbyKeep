9dc596e7f2e534c38e46b15340e8d8fea0bc01e2
James <615283@users.noreply.github.com>
2018-05-25 21:21:43 +0100

    checking method stubs in start cmd

--- src/main/java/com/georlegacy/general/lobbykeep/commands/PKStartCommand.java
+++ src/main/java/com/georlegacy/general/lobbykeep/commands/PKStartCommand.java
@@ -1,6 +1,7 @@
 package com.georlegacy.general.lobbykeep.commands;
 
 import com.georlegacy.general.lobbykeep.LobbyKeep;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +14,23 @@ public class PKStartCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length!=1) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eBC &7| &cYou have provided incorrect arguments. Correct usage is &7/pkstart <parkour>"));
+            return true;
+        }
+        boolean is = false;
+        for (String rpn : lk.registeredParkours) {
+            if (rpn.toLowerCase().equals(args[0].toLowerCase())) {
+                is = true;
+                break;
+            }
+        }
+        if (!is) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eBC &7| " + args[0] + " &cdoesn't appear to be a valid parkour. Please try again."));
+            return true;
+        }
+
+
         return true;
     }
 }
