fcfc85b8f8333de6ce82b46adfe0c664213f28c9
James <615283@users.noreply.github.com>
2018-05-26 09:34:07 +0100

    completed (i think)

--- src/main/java/com/georlegacy/general/lobbykeep/commands/PKStartCommand.java
+++ src/main/java/com/georlegacy/general/lobbykeep/commands/PKStartCommand.java
@@ -1,10 +1,14 @@
 package com.georlegacy.general.lobbykeep.commands;
 
 import com.georlegacy.general.lobbykeep.LobbyKeep;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PKStartCommand implements CommandExecutor {
     private LobbyKeep lk;
@@ -29,8 +33,12 @@ public class PKStartCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eBC &7| " + args[0] + " &cdoesn't appear to be a valid parkour. Please try again."));
             return true;
         }
-
-
+        Location start = new Location(
+                Bukkit.getServer().getWorld(lk.getParkourData().parkour.getString(args[0] + ".World")),
+                lk.getParkourData().parkour.getInt(args[0] + ".Start.X"),
+                lk.getParkourData().parkour.getInt(args[0] + ".Start.Y"),
+                lk.getParkourData().parkour.getInt(args[0] + ".Start.Z"));
+        ((Player) sender).teleport(start);
         return true;
     }
 }
